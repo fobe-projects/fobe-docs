16c43d26dd18bd0ccd079abc9778d219f4b1d09a
Chiho Sin <chihosin@icloud.com>
2025-07-21 15:27:17 +0800

    Refactor code structure.

--- sidebars/community.js
+++
@@ -1,9 +0,0 @@
-import { chapterTitle, hr } from "./utils";
-
-export const community = [
-  chapterTitle("developer/community/contributing", "Contributing", "community"),
-  hr(),
-  "developer/community/faq",
-  "developer/community/zero-downtime-migrations",
-  "developer/community/support",
-];
