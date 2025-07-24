83018af4094eca9995847bdaa2fa446fd9253914
Chiho Sin <chihosin@icloud.com>
2025-07-24 18:30:24 +0800

    docs: Update documentation structure and add new pages
    - Updated links in the index.mdx file for various products.
    - Added new documentation pages for Arduino, MicroPython, Zephyr RTOS, and several solutions.
    - Marked all new pages as under construction.
    
    Signed-off-by: ChihoSin chihosin@icloud.com

--- sidebars.js
+++ sidebars.js
@@ -30,17 +30,6 @@ export const ref = (id, label, icon) => {
   };
 };
 
-const backToHome = {
-  type: "ref",
-  id: "index",
-  label: "<- Back to home",
-  className: "menu__backlink",
-  customProps: {
-    sidebar_is_backlink: true,
-    sidebar_icon: "back-arrow",
-  },
-};
-
 module.exports = {
   main: [
     {
@@ -53,35 +42,153 @@ module.exports = {
     },
     {
       type: "category",
-      label: "Quickstart",
-      items: ["product/quill", "product/quill", "product/quill"],
+      label: "Solutions",
+      link: { type: "doc", id: "solutions/index" },
+      items: [
+        "solutions/meshbus",
+        "solutions/dtu",
+        "solutions/env-telemetry",
+        "solutions/dpc",
+        {
+          type: "category",
+          label: "Meshtastic",
+          link: { type: "doc", id: "solutions/meshtastic" },
+          items: [
+            "solutions/meshtastic/idea-cube",
+            "solutions/meshtastic/solar-power",
+          ],
+        },
+      ],
       customProps: {
-        icon: "play",
+        icon: "platform",
       },
     },
 
     title("Development Boards"),
-    ref("product/quill", "Quill Series", "feather"),
-    ref("product/base", "Base Series", "thermometer"),
-    ref("product/breakout", "Breakout Series", "appBoard"),
-    // ref("developer/index", "Pico Series", "distribute"),
-    // ref("cloud/overview", "Mesh Cube", "appBoard"),
+    {
+      type: "category",
+      label: "Quill Series",
+      link: { type: "doc", id: "product/quill" },
+      items: [
+        {
+          type: "category",
+          label: "Quill nRF52840 Mesh",
+          link: { type: "doc", id: "product/f1101/index" },
+          items: [
+            "product/f1101/quickstart",
+            "product/f1101/programming",
+            "product/f1101/applications",
+            "product/f1101/resources",
+          ],
+        },
+        {
+          type: "category",
+          label: "Quill ESP32-S3 Mesh",
+          link: { type: "doc", id: "product/f1102/index" },
+          items: [
+            "product/f1102/quickstart",
+            "product/f1102/programming",
+            "product/f1102/applications",
+            "product/f1102/resources",
+          ],
+        },
+      ],
+    },
+    // {
+    //   type: "category",
+    //   label: "Pico Series",
+    //   link: { type: 'doc', id: 'product/pico' },
+    //   items: [
+    //   ],
+    // },
+    // {
+    //   type: "category",
+    //   label: "DevKit Series",
+    //   link: { type: 'doc', id: 'product/devkit' },
+    //   items: [
+    //   ],
+    // },
+    {
+      type: "category",
+      label: "Idea Series",
+      link: { type: "doc", id: "product/idea" },
+      items: [
+        {
+          type: "category",
+          label: "Idea Quill Solar Power",
+          link: { type: "doc", id: "product/f2101/index" },
+          items: ["product/f2101/quickstart", "product/f2101/resources"],
+        },
+        {
+          type: "category",
+          label: "Idea Mesh Cube",
+          link: { type: "doc", id: "product/f2102/index" },
+          items: ["product/f2102/quickstart", "product/f2102/resources"],
+        },
+      ],
+    },
 
-    // title("Modules"),
-    // ref("cloud/overview", "F6001", "coreModule"),
+    title("Modules"),
+    {
+      type: "category",
+      label: "Core",
+      link: { type: "doc", id: "product/core" },
+      items: [
+        {
+          type: "category",
+          label: "F6001",
+          link: { type: "doc", id: "product/f6001/index" },
+          items: ["product/f6001/quickstart", "product/f6001/resources"],
+        },
+      ],
+    },
 
-    // title("Projects"),
-    // ref("developer/app-store/overview", "MeshCube for Meshtastic", "box"),
-    // ref("developer/app-store/overview", "Solar Power for Meshtastic", "box"),
+    title("Extensions"),
+    {
+      type: "category",
+      label: "Breakout",
+      link: { type: "doc", id: "product/breakout" },
+      items: [
+        {
+          type: "category",
+          label: "FoBE Breakout SHT40",
+          link: { type: "doc", id: "product/f2202/index" },
+          items: ["product/f2202/quickstart", "product/f2202/resources"],
+        },
+        {
+          type: "category",
+          label: "FoBE Breakout L76K",
+          link: { type: "doc", id: "product/f2207/index" },
+          items: ["product/f2207/quickstart", "product/f2207/resources"],
+        },
+        {
+          type: "category",
+          label: "FoBE Breakout ADS1115",
+          link: { type: "doc", id: "product/f2208/index" },
+          items: ["product/f2208/quickstart", "product/f2208/resources"],
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "Shield",
+      link: { type: "doc", id: "product/shield" },
+      items: [],
+    },
 
-    // title("Extensions"),
-    // ref("developer/app-store/overview", "Sensors", "thermometer"),
-    // ref("developer/app-store/overview", "Batteries", "charge"),
-
-    // title("Platforms"),
-    // ref("developer/app-store/overview", "Zephyr RTOS", "platform"),
-    // ref("developer/app-store/overview", "Arduino", "platform"),
-    // ref("developer/app-store/overview", "MicroPython", "platform"),
+    title("Platforms"),
+    {
+      type: "doc",
+      id: "platform/arduino",
+    },
+    {
+      type: "doc",
+      id: "platform/micropython",
+    },
+    {
+      type: "doc",
+      id: "platform/zephyr",
+    },
 
     title("Additional Resources"),
     {
@@ -128,85 +235,6 @@ module.exports = {
       customProps: {
         icon: "discord",
       },
-    },
-  ],
-  productQuill: [
-    backToHome,
-    chapterTitle("product/quill", "Quill Series", "box"),
-    {
-      type: "category",
-      label: "FoBE Quill nRF52840 Mesh",
-      items: [
-        "product/f1101/index",
-        "product/f1101/quickstart",
-        "product/f1101/programming",
-        "product/f1101/applications",
-        "product/f1101/resources",
-      ],
-    },
-    {
-      type: "category",
-      label: "FoBE Quill ESP32-S3 Mesh",
-      items: [
-        "product/f1102/index",
-        "product/f1102/quickstart",
-        "product/f1102/programming",
-        "product/f1102/applications",
-        "product/f1102/resources",
-      ],
-    },
-  ],
-  productBase: [
-    backToHome,
-    chapterTitle("product/base", "Base Series", "thermometer"),
-    {
-      type: "category",
-      label: "FoBE Base with Solar Power",
-      items: [
-        "product/f2101/index",
-        "product/f2101/quickstart",
-        "product/f2101/resources",
-      ],
-    },
-    {
-      type: "category",
-      label: "FoBE Base Cube nRF52840",
-      items: [
-        "product/f2102/index",
-        "product/f2102/quickstart",
-        "product/f2102/resources",
-      ],
-    },
-  ],
-  productBreakout: [
-    backToHome,
-    chapterTitle("product/breakout", "Breakout Series", "appBoard"),
-    {
-      type: "category",
-      label: "FoBE Breakout SHT40",
-      items: [
-        "product/f2202/index",
-        "product/f2202/quickstart",
-        "product/f2202/resources",
-      ],
-    },
-    {
-      type: "category",
-      label: "FoBE Breakout L76K",
-      items: [
-        "product/f2207/index",
-        "product/f2207/quickstart",
-        "product/f2207/resources",
-      ],
-    },
-    {
-      type: "category",
-      label: "FoBE Breakout ADS1115",
-      items: [
-        "product/f2208/index",
-        "product/f2208/quickstart",
-        "product/f2208/resources",
-      ],
     },
   ],
 };
