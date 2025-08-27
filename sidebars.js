7ccb5b4f9684f395c32f2fc8c8e7c5d465cb7963
Chiho Sin <chihosin@icloud.com>
2025-08-27 17:01:18 +0800

    docs: Update product section in documentation Reorganized the product section to improve clarity and added new product entries with "Coming soon" labels for upcoming items.
    Signed-off-by: Chiho Sin <chihosin@icloud.com>

--- sidebars.js
+++ sidebars.js
@@ -230,19 +230,19 @@ module.exports = {
     //   items: [],
     // },
 
-    title("Platforms"),
-    {
-      type: "doc",
-      id: "platform/arduino",
-    },
-    {
-      type: "doc",
-      id: "platform/micropython",
-    },
-    {
-      type: "doc",
-      id: "platform/zephyr",
-    },
+    // title("Platforms"),
+    // {
+    //   type: "doc",
+    //   id: "platform/arduino",
+    // },
+    // {
+    //   type: "doc",
+    //   id: "platform/micropython",
+    // },
+    // {
+    //   type: "doc",
+    //   id: "platform/zephyr",
+    // },
 
     title("Additional Resources"),
     {
